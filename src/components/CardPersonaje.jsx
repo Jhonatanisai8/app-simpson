590f2a580848da7685980b4be2ef198f6abbf1c5
jhonatansai <ojedasnachezjhonatan@gmail.com>
2026-02-14 16:19:30 -0500

    Se añadió la sección de visualización de imágenes y frases icónicas al componente CardPersonaje para una presentación mejorada de los detalles del personaje.

--- src/components/CardPersonaje.jsx
+++ src/components/CardPersonaje.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
 
 const CardPersonaje = ({ personaje }) => {
+  const frasesPersonaje = personaje.phrases;
   return (
     <div className="card__personaje">
       <h2 className="personaje__nombre">{personaje.name}</h2>
+      <div className="personaje__img">
+        <img src={`https://cdn.thesimpsonsapi.com/500${personaje.portrait_path}`} alt="Img Personaje" />
+      </div>
       <div className="personaje__datos-personales">
         <ul className="listado__datos">
           <li className="listad__item"><p>Edad: {personaje.age}</p></li>
@@ -11,6 +15,14 @@ const CardPersonaje = ({ personaje }) => {
           <li className="listad__item"><p>Genero: {personaje.gender}</p></li>
           <li className="listad__item"><p>Ocupacion: {personaje.occupation}</p></li>
         </ul>
+      </div>
+      <div className="personaje__frases">
+        <h3>Frases Iconicas: </h3>
+        <ol>
+          {frasesPersonaje.map((frase,index) => (
+            <li key={index}>{frase}</li>))
+          }
+        </ol>
       </div>
     </div>
   )
